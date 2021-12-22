23db56d71222be6c3fdd95628ea9e855f4c34865
dirayser <dirayser@gmail.com>
2021-12-22 20:58:30 +0200

    implement client side

---
+++ client/common/http.js
@@ -0,0 +1,30 @@
+const request = require('request');
+
+const Client = (baseUrl) => {
+    return {
+        get: (path) => {
+            return new Promise((resolve, reject) => {
+                request(`${baseUrl}${path}`, { json: true }, (err, res, body) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    resolve(body);
+                });
+            });
+        },
+        post: async(path, data) => {
+            return new Promise((resolve, reject) => {
+                request(`${baseUrl}${path}`, { json: true, method: 'POST', body: data }, (err, res, body) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    resolve(body);
+                });
+            });
+        }
+    };
+};
+
+module.exports = { Client };
